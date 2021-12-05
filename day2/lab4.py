3dfbd9b9e4626b71aa7bdd48e5b77d4c74ebe0d7
sarinamagar <msarina823@gmail.com>
2021-12-05 11:22:39 +0545

    lab work

--- day2/lab4.py
+++ day2/lab4.py
@@ -1,4 +1,9 @@
-#apples
+
+#N students take K apples and distribute them among each other evenly. The remaining
+#the indivisible) part remains in the basket. How many apples will each single student
+#get? How many apples will remain in the basket? The program reads the numbers N and
+#K. It should print the two answers for the questions above.
+
 n= int(input("no. of students ="))
 k= int(input("no of apples ="))
 apples = k//n
